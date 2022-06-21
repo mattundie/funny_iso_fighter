b6fb21d26db8024d4631465cba26512bdc6409c0
Matt Underation <mattunderation@gmail.com>
2022-06-21 17:34:23 -0400

    Added dynamic jumping mechanics, tuned movement, edited test scene

--- Assets/Scripts/Player/PlayerMovementNew.cs
+++ Assets/Scripts/Player/PlayerMovementNew.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using UnityEngine.SceneManagement;
 using UnityEngine.Animations.Rigging;
 using RootMotion.Dynamics;
+using System;
 
 public class PlayerMovementNew : MonoBehaviour
 {
@@ -18,28 +19,38 @@ public class PlayerMovementNew : MonoBehaviour
 
     [Header("Move Settings")]
     [SerializeField] private float _maxSpeed = 8f;
-    [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private float _acceleration = 200f;
     [SerializeField] private AnimationCurve _accelerationFactorFromDot;
     [SerializeField] private float _maxAccelForce = 400f;
     [SerializeField] private AnimationCurve _maxAccelerationForceFactorFromDot;
     [SerializeField] private Vector3 _forceScale = new Vector3(1, 0, 1);
-    [SerializeField] private float _gravityScaleDrop = 10f;
 
     [Header("Hover Settings")]
     [SerializeField] private float _rotateSpeed = 500f;
     [SerializeField] private float _rideHeight = 0.9f;
-    [SerializeField] private float _rideSpringStrength = 400f;
+    [SerializeField] private float _rideSpringStrength = 300f;
     [SerializeField] private float _rideSpringDamper = 50f;
 
+    [Header("Jump Settings")]
+    [SerializeField] private float _initialJumpForceMultiplier = 750f;
+    [SerializeField] private float _continualJumpForceMultiplier = 0.1f;
+    [SerializeField] private float _jumpTime = 0.175f;
+    [SerializeField] private float _jumpTimeCounter = 0f;
+    [SerializeField] private float _coyoteTime = 0.15f;
+    [SerializeField] private float _coyoteTimeCounter = 0f;
+    [SerializeField] private float _jumpBufferTime = 0.2f;
+    [SerializeField] private float _jumpBufferTimeCounter = 0f;
+    [SerializeField] private bool _jumpWasPressedLastFrame = false;
+
     [Header("Monitored Data")]
-    [SerializeField] private bool _isMoving;
+    [SerializeField] private bool _isMoving = false;
+    [SerializeField] private bool _isJumping = false;
     [SerializeField] private bool _grounded;
 
     private Vector3 _buttonInput;
     private RaycastHit _raycastHit;
     private bool _pressedJump;
-    private bool _disableHover;
+    private float _pressedJumpTimer;
 
     private bool IsMouseOverGameWindow { get { return !(0 > Input.mousePosition.x || 0 > Input.mousePosition.y || Screen.width < Input.mousePosition.x || Screen.height < Input.mousePosition.y); } }
 
@@ -63,9 +74,9 @@ public class PlayerMovementNew : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Jump();
         Hover();
         Move();
-        Jump();
     }
 
     void GatherInput()
@@ -74,32 +85,36 @@ public class PlayerMovementNew : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            if (!_disableHover)
-            {
-                _pressedJump = true;
-                _disableHover = true;
-                Invoke("HoverBufferReset", 0.20f);
-            }
+            _pressedJump = true;
+            _pressedJumpTimer = Time.time;
         }
-    }
+        else if (Input.GetButton("Jump"))
+        {
+            _pressedJump = true;
 
-    void HoverBufferReset()
-    {
-        _disableHover = false;
+            if (Time.time - _pressedJumpTimer > _jumpTime)
+                _pressedJump = false;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            _pressedJump = false;
+            _pressedJumpTimer = 0f;
+        }
     }
 
     #region Movement Functions
     void Hover()
     {
-        if (_puppet.state != BehaviourPuppet.State.Puppet || _disableHover)
-            return;
-
-        bool didRaycastHit = Physics.Raycast(_raycastCenter.position, Vector3.down, out _raycastHit, _rideHeight + 0.5f, _raycastLayers);
+        bool didRaycastHit = Physics.Raycast(_raycastCenter.position, Vector3.down, out _raycastHit, _rideHeight + 0.2f, _raycastLayers);
         Debug.DrawRay(_raycastCenter.position, Vector3.down * _rideHeight, Color.red);
 
         if (didRaycastHit)
         {
             _grounded = true;
+
+            if (_puppet.state != BehaviourPuppet.State.Puppet || _isJumping)
+                return;
+
             Vector3 vel = _rb.velocity;
             Vector3 rayDir = transform.TransformDirection(Vector3.down);
 
@@ -197,15 +212,30 @@ public class PlayerMovementNew : MonoBehaviour
 
     void Jump()
     {
-        if (_puppet.state != BehaviourPuppet.State.Puppet)
-            return;
+        float calculatedJumpInput;
 
-        if (_pressedJump)
+        SetJumpTimeCounter();
+        SetCoyoteTimeCounter();
+        SetJumpBufferCounter();
+
+        if (_pressedJump && _jumpBufferTimeCounter > 0.0f && !_isJumping && _coyoteTimeCounter > 0.0f)
         {
-            _animator.SetTrigger("jump");
+            calculatedJumpInput = _initialJumpForceMultiplier;
+            _rb.AddForce(new Vector3(0, calculatedJumpInput, 0), ForceMode.Impulse);
 
-            _rb.AddForce(new Vector3(0, _jumpForce, 0), ForceMode.Impulse);
-            _pressedJump = false;
+            _animator.SetTrigger("jump");
+            _jumpBufferTimeCounter = 0;
+            _coyoteTimeCounter = 0;
+            _isJumping = true;
+        }
+        else if (_pressedJump && _isJumping && !_grounded && _jumpTimeCounter > 0.0f)
+        {
+            calculatedJumpInput = _initialJumpForceMultiplier * _continualJumpForceMultiplier;
+            _rb.AddForce(new Vector3(0, calculatedJumpInput, 0), ForceMode.Force);
+        }
+        else if(_isJumping && _grounded && !_pressedJump)
+        {
+            _isJumping = false;
         }
     }
 
@@ -219,6 +249,43 @@ public class PlayerMovementNew : MonoBehaviour
 
         float clampedVelocity = _rb.velocity.magnitude / _maxSpeed;
         _animator.SetFloat("speed", clampedVelocity);
+    }
+    #endregion
+
+    #region Helper Functions
+    private void SetJumpTimeCounter()
+    {
+        if(_isJumping && !_grounded)
+        {
+            _jumpTimeCounter -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            _jumpTimeCounter = _jumpTime;
+        }
+    }
+    private void SetCoyoteTimeCounter()
+    {
+        if (_grounded)
+        {
+            _coyoteTimeCounter = _coyoteTime;
+        }
+        else
+        {
+            _coyoteTimeCounter -= Time.fixedDeltaTime;
+        }
+    }
+    private void SetJumpBufferCounter()
+    {
+        if(!_jumpWasPressedLastFrame && _pressedJump)
+        {
+            _jumpBufferTimeCounter = _jumpBufferTime;
+        }
+        else if(_jumpBufferTime > 0.0f)
+        {
+            _jumpBufferTimeCounter -= Time.fixedDeltaTime;
+        }
+        _jumpWasPressedLastFrame = _pressedJump;
     }
     #endregion
 }
